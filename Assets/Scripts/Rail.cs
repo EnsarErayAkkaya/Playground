6ec9a26206d27a61c356e10ec917e12953bae471
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2020-06-03 00:57:21 +0300

    track adding added

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BezierSolution;
+using UnityEngine;
+
+public class Rail : MonoBehaviour
+{
+    BezierSpline splineManager;
+    RailManager railManager;
+
+    public Vector3 endPoint;
+    
+    [SerializeField] bool isStart;
+    void Start()
+    {
+        splineManager = FindObjectOfType<BezierSpline>();
+        railManager = FindObjectOfType<RailManager>();
+        if(!isStart)
+            ConnectRailToClosest();
+    }
+    // ADD TRAIN TRACK
+    void AddBezierSplinePoint()
+    {
+
+    }
+    // ROTATE RAIL
+    public void RotateRail()
+    {
+
+    }
+    // DELETE RAIL
+    public void DeleteRail()
+    {
+
+    }
+
+    public void ConnectRailToClosest()
+    {
+        Rail connectingRail = railManager.rails[railManager.rails.Count-1];
+
+        transform.position = connectingRail.transform.position + connectingRail.transform.right * connectingRail.endPoint.x;
+        transform.rotation = connectingRail.transform.rotation;
+        railManager.rails.Add(this);
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position,transform.right * endPoint.x);
+    }
+}
---
+++ Assets/Scripts/RailManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class RailManager : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    public float lineHeight, connectionDistance;
+    public List<Rail> rails;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Rail r = Instantiate(prefab).GetComponent<Rail>();
+        }
+    }
+}
