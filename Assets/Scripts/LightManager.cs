408788662ab716c30adc5a29d6a21f9b2d39d463
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2020-06-28 23:48:47 +0300

    Highlight prefab and material added
    prefab and material added and Light Manager script added

---
+++ Assets/Scripts/LightManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    [SerializeField] Light directionalLight;
+
+    public void CloseLights()
+    {
+        directionalLight.enabled = false;
+    }
+    public void OpenLights()
+    {
+        directionalLight.enabled = true;
+    }
+}
